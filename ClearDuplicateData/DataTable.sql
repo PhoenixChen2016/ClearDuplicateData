81b16ff2db28c40443b60f9beb5a3e0210fc5d2d
Phoenix <phoenix.chen@evertrust.com.tw>
2016-10-13 11:27:21 +0800

    建立測試資料庫

---
+++ ClearDuplicateData/DataTable.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[DataTable]
+(
+	[Id] INT NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL 
+)
---
+++ ClearDuplicateData/Script.PostDeployment1.sql
@@ -0,0 +1,23 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+INSERT INTO DataTable(Id, Name)
+VALUES(1, N'Test1'),
+	(1, N'Test1'),
+	(1, N'Test1'),
+	(2, N'Test2'),
+	(2, N'Test2'),
+	(3, N'Test3'),
+	(3, N'Test3'),
+	(3, N'Test3'),
+	(3, N'Test3'),
+	(4, N'Test4'),
+	(5, N'Test5');
