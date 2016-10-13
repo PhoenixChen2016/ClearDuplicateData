b85bca85729e0a321a8289d6076f3ff87a0bda88
Phoenix <phoenix.chen@evertrust.com.tw>
2016-10-13 11:32:22 +0800

    加入去除重複的SQL

--- ClearDuplicateData/Solution.sql
+++ ClearDuplicateData/Solution.sql
@@ -1,1 +1,19 @@
-﻿
+﻿WITH CTE
+AS
+(
+	SELECT
+		*,
+		Row_number() OVER(
+			PARTITION BY 
+				Id,
+				Name
+			ORDER BY
+				Id,
+				Name
+		) AS [RowNumber]
+	FROM
+		DataTable
+)
+DELETE CTE
+WHERE
+	[RowNumber] >  1;
